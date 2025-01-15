6a91062a79fed98ced10b93643c4666f1abf6740
idara ubom <winifredubom@gmail.com>
2025-01-15 11:23:27 +0100

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:habit_tracker/modules/screens/home_page.dart';
+import 'package:habit_tracker/firebase_options.dart';
+import 'package:habit_tracker/modules/screens/home/home_page.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
+
+
 
 
 class MyApp extends StatelessWidget {
